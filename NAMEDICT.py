0273c06f05025aaa7a09041dfa1c66da8b34ad6d
unknown <chensiyuan15@mails.ucas.ac.cn>
2017-06-30 08:55:15 +0800

    fix bug

---
+++ NAMEDICT.py
@@ -0,0 +1,38 @@
+# encoding: utf-8
+'''
+Created on 2017年5月24日
+
+@author: c
+'''
+NameDict=dict()
+NameDict['0']=0
+NameDict['1']=1
+NameDict['2']=2
+NameDict['101']=3
+NameDict['102']=1
+NameDict['103']=4
+NameDict['104']=5
+NameDict['105']=6
+NameDict['106']=7
+NameDict['107']=1
+NameDict['108']=8
+NameDict['109']=9
+NameDict['110']=10
+NameDict['201']=11
+NameDict['203']=12
+NameDict['204']=13
+NameDict['205']=2
+NameDict['209']=14
+NameDict['210']=15
+NameDict['211']=16
+NameDict['301']=17
+NameDict['303']=18
+NameDict['401']=19
+NameDict['402']=20
+NameDict['403']=21
+NameDict['405']=22
+NameDict['406']=23
+NameDict['407']=24
+NameDict['408']=25
+NameDict['409']=26
+NameDict['503']=27
